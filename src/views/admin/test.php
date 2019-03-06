d9c51c6e352856a87301d6419454751a82b79480
technote <technote.space@gmail.com>
2019-03-06 23:05:51 +0900

    v0.0.10

--- src/views/admin/test.php
+++ src/views/admin/test.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Test Views Admin Test
  *
- * @version 0.0.8
+ * @version 0.0.10
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
