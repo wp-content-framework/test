83ef1f163b2ce617a094668e77f304ad9313d0be
technote <technote.space@gmail.com>
2019-03-15 13:08:03 +0900

    v0.0.11

--- src/classes/controllers/admin/test.php
+++ src/classes/controllers/admin/test.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Test Classes Controller Test
  *
- * @version 0.0.9
+ * @version 0.0.11
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
